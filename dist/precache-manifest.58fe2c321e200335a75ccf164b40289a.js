1729d0f71c9c4eacdc22bc8adc7bd9f78fb91b50
S1NC0S <s1nc0s@protonmail.com>
2021-07-07 18:43:50 +0200

    add dist folder for github pages

---
+++ dist/precache-manifest.58fe2c321e200335a75ccf164b40289a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3c3821ff906326905e",
+    "url": "/css/app.c7a6fe59.css"
+  },
+  {
+    "revision": "ed10084e649f127afaab9079df396aa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c3c3821ff906326905e",
+    "url": "/js/app.6146db63.js"
+  },
+  {
+    "revision": "2541f354dfd79eee6084",
+    "url": "/js/chunk-vendors.0b75d493.js"
+  },
+  {
+    "revision": "2d2b2f18a50afd456132e57a9e03b430",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
